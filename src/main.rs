dcfbeead16e7882db029264b093699bd5cc6d84e
AloizioMacedo <atsam2@hotmail.com>
2024-01-13 21:08:54 -0300

    Add dotenv

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod product;
 use std::sync::Arc;
 
 use axum::Router;
+use dotenvy::dotenv;
 use mongodb::options::{ClientOptions, Credential};
 use mongodb::Client;
 
@@ -13,18 +14,25 @@ const DB_NAME: &str = "backend";
 
 #[tokio::main]
 async fn main() {
+    _ = dotenv();
+
+    eprintln!("Starting server...");
+    let mongo_service = std::env::var("MONGO_SERVICE").unwrap();
+    let username = std::env::var("MONGO_USER").unwrap();
+    let password = std::env::var("MONGO_PASSWORD").unwrap();
+
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .init();
 
-    let mut client_options = ClientOptions::parse("mongodb://localhost:27017")
+    let mut client_options = ClientOptions::parse(format!("mongodb://{mongo_service}:27017"))
         .await
         .unwrap();
 
     client_options.credential = Some(
         Credential::builder()
-            .username("root".to_string())
-            .password("example".to_string())
+            .username(username)
+            .password(password)
             .build(),
     );
 
@@ -37,5 +45,6 @@ async fn main() {
         .layer(tower_http::trace::TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
     axum::serve(listener, app).await.unwrap();
 }
